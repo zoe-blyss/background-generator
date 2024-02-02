944f69fd506ef2f953f7ad0542ff4436e132719c
Zoe Blyss Alexander <zoealexande@gmail.com>
2024-02-02 15:16:04 -0700

    added event listener for color inputs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ---------- defining variables ----------
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -5,3 +7,14 @@ var color2 = document.querySelector(".color2");
 console.log(css);
 console.log(color1);
 console.log(color2);
+
+
+// ---------- adding event listener for color inputs ----------
+
+color1.addEventListener("input", function(){
+	console.log(color1.value);
+});
+
+color2.addEventListener("input", function(){
+	console.log(color2.value);
+});
