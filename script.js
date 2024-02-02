d65ec82693d13d9f8646d09c39a32c1f2a8046ef
Zoe Blyss Alexander <zoealexande@gmail.com>
2024-02-02 15:12:35 -0700

    Defining variables JS

---
+++ script.js
@@ -0,0 +1,7 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+console.log(css);
+console.log(color1);
+console.log(color2);
